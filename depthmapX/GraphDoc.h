d2237208e7390c5c9c26faae38e451d7de2b0a35
Petros Koutsolampros <orange-vertex@users.noreply.github.com>
2018-03-16 03:14:33 +0000

    Remove various comments and dead code

--- depthmapX/GraphDoc.h
+++ depthmapX/GraphDoc.h
@@ -73,12 +73,12 @@ class CMSCommunicator : public Communicator
 {
 public:
    enum { IMPORT, IMPORTMIF, MAKEPOINTS, MAKEGRAPH, ANALYSEGRAPH, 
-          POINTDEPTH, METRICPOINTDEPTH, ANGULARPOINTDEPTH, TOPOLOGICALPOINTDEPTH, ANALYSEANGULAR, 
+          POINTDEPTH, METRICPOINTDEPTH, ANGULARPOINTDEPTH, TOPOLOGICALPOINTDEPTH,
           MAKEISOVIST, MAKEISOVISTPATH,
-          MAKEAXIALLINES, MAKEALLLINEMAP, MAKEFEWESTLINEMAP, MAKEDRAWING,
+          MAKEALLLINEMAP, MAKEFEWESTLINEMAP, MAKEDRAWING,
           MAKEUSERMAP, MAKEUSERMAPSHAPE, MAKEUSERSEGMAP, MAKEUSERSEGMAPSHAPE, MAKEGATESMAP, MAKEBOUNDARYMAP, MAKESEGMENTMAP, 
           MAKECONVEXMAP, 
-          AXIALANALYSIS, SEGMENTANALYSIS, TOPOMETANALYSIS, AGENTANALYSIS, IMPORTEDANALYSIS };
+          AXIALANALYSIS, SEGMENTANALYSIS, TOPOMETANALYSIS, AGENTANALYSIS };
 	
 public:
    void ProcPostMessage(int m, int x, int y);
@@ -118,20 +118,6 @@ public:
    { m_string= str; }
    const QString& GetString() const
    { return m_string; }
-
-   //
-   void SetFileSet(QStringList strings)
-   { 
-/*	   m_fileset.clear();
-	   comm_string strs;
-       for (int i = 0; i < strings.size(); i++)
-	   {
-		   strs.clear();
-           for(int j=0; j<strings[i].size(); j++) strs.push_back(strings[i].toLatin1().data()[j]);
-           m_fileset.push_back(strs);
-       }*/
-   }
-
 
 protected:
    pvecint m_options;
@@ -264,11 +250,6 @@ public:
    void OnFillPoints(const Point2f& p, int fill_type = 0 );
    void OnMakeIsovist(const Point2f& seed, double angle = -1.0);
    void OnToolsAxialMap( const Point2f& seed );
-   //
-   bool CheckMemory(const QString& filename = QString());
-   //
-   bool ViewHandler(int nCode, void *pExtra, int viewing, int layer = -1);
-   //
    int RenameColumn(AttributeTable *tab, int col);
    bool ReplaceColumnContents(PointMap* pointmap, ShapeMap *shapemap, int col);
    bool SelectByQuery(PointMap* pointmap, ShapeMap *shapemap);
@@ -288,16 +269,12 @@ public:
     void OnAxialConnectionsExportAsPairCSV();
     void OnSegmentConnectionsExportAsPairCSV();
 	void OnToolsMakeGraph();
-	void OnEditClear();
-	void OnVGAOptions();
+    void OnEditClear();
 	void OnToolsRun();
 	void OnEditUndo();
     void OnToolsPD();
 	void OnPushToLayer();
-	void OnEditGrid();
-	void OnToolsAxialLines();
-	void OnEditFixgrid();
-	void OnEditFixFill();
+    void OnEditGrid();
     void OnToolsMPD();
 	void OnToolsMakeFewestLineMap();
 	void OnToolsRunSeg();
@@ -306,18 +283,12 @@ public:
 	void OnFileProperties();
 	void OnToolsAPD();
 	void OnViewShowGrid();
-	void OnViewSummary();
-	void OnViewShowText();
+    void OnViewSummary();
 	void OnToolsRunAxa();
     void OnSwapColours();
-	void OnToolsAxialClearLinks();
 	void OnToolsPointConvShapeMap();
 	void OnToolsAxialConvShapeMap();
-	void OnUpdateColumn();
-	void OnWindow3dView();
-	void OnViewScatterplot();
-	void OnWindowMap();
-	void OnViewTable();
+    void OnUpdateColumn();
     void OnToolsAgentRun();
 	void OnRenameColumn();
 	void OnEditQuery();
@@ -330,8 +301,7 @@ public:
     void OnToolsIsovistpath();
     bool OnFileSave();
     bool OnFileSaveAs();
-	void OnConvertMapShapes();
-	void OnToolsBoundaryToAxial();
+    void OnConvertMapShapes();
 	void OnToolsLineLoadUnlinks();
     void OnPointmapExportConnectionsAsCSV();
 
