3eedc6bf2eae51261f1a9b3b1e91e793f605ed79
Christian Sailer <sar@mailraum.de>
2019-04-18 09:47:17 +0100

    Add labels/steps to build

--- ci/build.sh
+++ ci/build.sh
@@ -6,8 +6,15 @@ if [ ! -d build ]; then
 fi
 cd build
 # build
+echo Building
 cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . || exit 1
 # if succeeds, run unit tests
+echo Running unit tests
 ./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./salaTest/salaTest && ./genlibTest/genlibTest || exit 1
 # if that succeeds, run regression tests
-cd ../RegressionTest/test && python3.5 test_main.py && cd .. && python3.5 RegressionTestRunner.py && python3.5 RegressionTestRunner.py regressionconfig_agents.json
+echo testing regression test framework
+cd ../RegressionTest/test && python3.5 test_main.py || exit 1
+echo running standard regression tests
+cd .. && python3.5 RegressionTestRunner.py || exit 1
+echo running agent test
+python3.5 RegressionTestRunner.py regressionconfig_agents.json || exit 1
