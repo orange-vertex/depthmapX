0a13d9f3586cf371c8d55099178da6bf6447af19
Christian Sailer <sar@mailraum.de>
2019-04-18 09:19:31 +0100

    Flag to hopefully make cmake work with ubuntu 16.04 in docker and move everything into build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 source /opt/qt511/bin/qt511-env.sh
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+if [ ! -d build ]; then
+    mkdir build
+fi
+cd build
+# build
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . || exit 1
+# if succeeds, run unit tests
+./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./salaTest/salaTest && ./genlibTest/genlibTest || exit 1
+# if that succeeds, run regression tests
+cd ../RegressionTest/test && python3.5 test_main.py && cd .. && python3.5 RegressionTestRunner.py && python3.5 RegressionTestRunner.py regressionconfig_agents.json
