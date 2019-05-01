bcdce4647243a9caedb85a43d04c757e9806c3a4
Christian Sailer <sar@mailraum.de>
2019-05-01 21:29:30 +0100

    Use unbuffered python output

--- ci/build.sh
+++ ci/build.sh
@@ -13,8 +13,8 @@ echo Running unit tests
 ./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./salaTest/salaTest && ./genlibTest/genlibTest || exit 1
 # if that succeeds, run regression tests
 echo testing regression test framework
-cd ../RegressionTest/test && echo pwd && python3.5 test_main.py || exit 1
+cd ../RegressionTest/test && echo pwd && python3.5 -u test_main.py || exit 1
 echo running standard regression tests
-cd .. && pwd && python3.5 RegressionTestRunner.py || exit 1
+cd .. && pwd && python3.5 -u RegressionTestRunner.py || exit 1
 echo running agent test
-python3.5 RegressionTestRunner.py regressionconfig_agents.json || exit 1
+python3.5 -u RegressionTestRunner.py regressionconfig_agents.json || exit 1
